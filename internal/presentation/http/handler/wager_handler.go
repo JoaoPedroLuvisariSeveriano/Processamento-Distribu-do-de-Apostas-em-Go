c262ffc5514db66dc456896f7440cfbefe361bf2
Joao Pedro Luvisari <joaopedroluvisariseveriano@gmail.com>
2026-09-18 22:56:25 -0300

    fix(api): adiciona import do chi nos handlers para correcao do build

--- internal/presentation/http/handler/wager_handler.go
+++ internal/presentation/http/handler/wager_handler.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 	"go.uber.org/zap"
 
--- internal/presentation/http/handler/wallet_handler.go
+++ internal/presentation/http/handler/wallet_handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 	"go.uber.org/zap"
 
